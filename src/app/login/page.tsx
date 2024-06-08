a4b767992ef07f4c0783086d291238a69dc74cd5
Brandon <25nguyenb26@stu.smuhsd.org>
2024-06-08 08:45:07 -0700

    styling login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,12 @@
 const login = () => {
-    return <div>Hellow</div>
+    return <main className="h-screen p-24">
+        <div className="w-1/3 h-full m-auto bg-white">
+        
+        <h1 className="">Log in </h1>
+        
+        </div>
+        
+        </main>
 }
 
 export default login
