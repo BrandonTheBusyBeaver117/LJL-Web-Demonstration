8a947d6ef2f1fd0fb1457c771fb8c952cb7e2215
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2024-06-09 23:00:04 -0700

    no need for card

--- src/app/components/card.tsx
+++
@@ -1,9 +0,0 @@
-// Wrapper class for dashboard components
-
-const Card = ({ children }: { children: React.ReactNode }) => {
-    return <div className="flex justify-center items-center">
-        {children}
-    </div>
-}
-
-export default Card
