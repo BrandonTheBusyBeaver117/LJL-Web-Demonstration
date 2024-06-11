decdf5c1deefee780e043ebac370655b8845dafc
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2024-06-10 23:03:54 -0700

    further styling dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -40,32 +40,30 @@ const dashboard: React.FC<DashboardProps> = ({ params }) => {
         <>
             <button
                 onClick={() => signOut(auth)}
-                className="absolute top-4 right-4 bg-fuchsia-500 text-white py-2 px-4 rounded-lg hover:bg-pink-500 transition ease-in-out duration-300"
+                className="absolute top-4 right-4 bg-fuchsia-500 border-2 border-fuchsia-700 text-white py-2 px-4 rounded-lg hover:bg-pink-500 transition ease-in-out duration-300"
             >
                 Sign Out
             </button>
 
-            <div className=" bg-sky-200 flex flex-col items-center justify-evenly grow rounded-3xl border-4 border-sky-600">
+            <div className="w-1/2 h-full bg-sky-200 flex flex-col items-center justify-evenly rounded-3xl border-4 border-sky-600">
 
-                <div id="intro wrapper" className="flex flex-col items-center text-3xl grow-[3] basis-0">
+                <div id="intro wrapper" className="flex flex-col items-center text-2xl grow-[3] basis-0">
                     <h1 className="m-5">Hello {name}!</h1>
                     <h1>Welcome to your dashboard!</h1>
                 </div>
 
 
-                <div className="flex flex-col items-center text-1xl grow-[5] basis-0">
-                    <h2 className="m-5">Chances of allowing Brandon to intern at LJL:</h2>
-                    <div id="image wrapper" className="flex flex-col items-center">
-                        <Image
-                            className="relative"
-                            src="/upwardtrend.jpg"
-                            alt="chart"
-                            width={0}
-                            height={0}
-                            sizes="100vw"
-                            style={{ width: '50%', height: 'auto' }}
-                        />
-                    </div>
+                <h2 className="m-5">Chances of allowing Brandon to intern at LJL:</h2>
+                <div className="w-3/4 flex flex-col items-center text-1xl grow-[5] basis-0 relative mb-6">
+
+                    <Image
+                        className="relative"
+                        src="/upwardtrend.jpg"
+                        alt="chart"
+                        fill={true}
+                        style={{ objectFit: "contain" }}
+                    />
+
                 </div>
             </div>
         </>)
