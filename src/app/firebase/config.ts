a5e07933e7c2a6f3cf3f91eefb2f4436d3c64c0f
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2024-06-10 23:56:20 -0700

    adding keys, because apparently it's safe

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+    apiKey: "AIzaSyB8Qg1wo205gMOozlkWCCKDVeP2woKg5sA",
+    authDomain: "ljl-demo.firebaseapp.com",
+    projectId: "ljl-demo",
+    storageBucket: "ljl-demo.appspot.com",
+    messagingSenderId: "696926417854",
+    appId: "1:696926417854:web:6187dd013136d27810bfef"
 };
 
 // Server side rendering trickery
