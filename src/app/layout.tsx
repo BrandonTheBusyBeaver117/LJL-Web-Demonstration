ada6d805dd7842bc30ca04dc0ad8f29806c94f49
Brandon Nguyen <25nguyenb26@stu.smuhsd.org>
2024-06-11 09:18:08 -0700

    adding favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <header className="flex h-[12.5vh] items-center justify-center">
           <Link href="/">
